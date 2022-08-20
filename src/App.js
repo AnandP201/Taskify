7f2ad792dca886560794d7c9ef1008fa3c843cd4
Anand <pie1002anand@gmail.com>
2022-08-20 08:42:19 +0530

    created taskify app and deployed to github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import './App.css'
+import 'rsuite/dist/rsuite.min.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
